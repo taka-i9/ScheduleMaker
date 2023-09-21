a18ef9bc8ac5e670abc67b0f02484a707df40242
taka-i9 <taka.9.hiro.17@gmail.com>
2023-09-22 00:18:00 +0900

    add workflow contents' move

--- resources/views/workflowEditForm.blade.php
+++ resources/views/workflowEditForm.blade.php
@@ -56,43 +56,101 @@
     var content_id = 1;
     var connect_begin_id = "";
     var connect_relation = {};
+    var connect_relation_rev = {};
+    var movingContent = "";
+    var block_once = false;
+
+    function addForm(id) {
+        let titleData = document.createElement("input");
+        titleData.type = "hidden";
+        titleData.name = id_head + id + "_title_form";
+        titleData.id = id_head + id + "_title_form";
+        titleData.value = document.getElementById(id_head + id + "_title").innerText;
+        let hourData = document.createElement("input");
+        hourData.type = "hidden";
+        hourData.name = id_head + id + "_time_hour_form";
+        hourData.id = id_head + id + "_time_hour_form";
+        hourData.value = document.getElementById(id_head + id + "_time_hour").innerText;
+        let minuteData = document.createElement("input");
+        minuteData.type = "hidden";
+        minuteData.name = id_head + id + "_time_minute_form";
+        minuteData.id = id_head + id + "_time_minute_form";
+        minuteData.value = document.getElementById(id_head + id + "_time_minute").innerText;
+        let marginLeftData = document.createElement("input");
+        marginLeftData.type = "hidden";
+        marginLeftData.name = id_head + id + "_margin_left_form";
+        marginLeftData.id = id_head + id + "_margin_left_form";
+        marginLeftData.value = document.getElementById(id_head + id).style.marginLeft;
+        let marginTopData = document.createElement("input");
+        marginTopData.type = "hidden";
+        marginTopData.name = id_head + id + "_margin_top_form";
+        marginTopData.id = id_head + id + "_margin_top_form";
+        marginTopData.value = document.getElementById(id_head + id).style.marginTop;
+        document.getElementById("workflow_data").appendChild(titleData);
+        document.getElementById("workflow_data").appendChild(hourData);
+        document.getElementById("workflow_data").appendChild(minuteData);
+        document.getElementById("workflow_data").appendChild(marginLeftData);
+        document.getElementById("workflow_data").appendChild(marginTopData);
+    }
+
+    function removeConnection(id) {
+        if(movingContent in connect_relation) {
+            connect_relation[movingContent].forEach(function(v) {
+                document.getElementById("connection_" + id + "_" + v).remove();
+            });
+        }
+        if(movingContent in connect_relation_rev) {
+            connect_relation_rev[movingContent].forEach(function(v) {
+                document.getElementById("connection_" + v + "_" + id).remove();
+            });
+        }
+    }
+
+    function attachConnection(id) {
+        if(movingContent in connect_relation) {
+            connect_relation[movingContent].forEach(function(v) {
+                createArrow(id, v);
+            });
+        }
+        if(movingContent in connect_relation_rev) {
+            connect_relation_rev[movingContent].forEach(function(v) {
+                createArrow(v, id);
+            });
+        }
+    }
 
     var moveContent = function(e) {
         let target_rect = e.currentTarget.getBoundingClientRect();
         let x = e.clientX - target_rect.left;
         let y = e.clientY - target_rect.top;
-        document.getElementById("registration_free_field").lastElementChild.style.marginLeft  = String(x) + "px";
-        document.getElementById("registration_free_field").lastElementChild.style.marginTop = String(y) + "px";
+        document.getElementById(id_head + movingContent).style.marginLeft  = String(x) + "px";
+        document.getElementById(id_head + movingContent).style.marginTop = String(y) + "px";
     };
 
     var putContent = function(e) {
+        if(block_once) {
+            block_once = false;
+            return;
+        }
         document.getElementById("registration_free_field").removeEventListener("mousemove", moveContent);
         document.getElementById("registration_free_field").removeEventListener("click", putContent);
-        document.getElementById("registration_menu_" + mode).style.backgroundColor = "";
-        let titleData = document.createElement("input");
-        titleData.type = "hidden";
-        titleData.name = id_head + String(content_id) + "_title_form";
-        titleData.id = id_head + String(content_id) + "_title_form";
-        titleData.value = document.getElementById(id_head + String(content_id) + "_title").innerText;
-        let hourData = document.createElement("input");
-        hourData.type = "hidden";
-        hourData.name = id_head + String(content_id) + "_time_hour_form";
-        hourData.id = id_head + String(content_id) + "_time_hour_form";
-        hourData.value = document.getElementById(id_head + String(content_id) + "_time_hour").innerText;
-        let minuteData = document.createElement("input");
-        minuteData.type = "hidden";
-        minuteData.name = id_head + String(content_id) + "_time_minute_form";
-        minuteData.id = id_head + String(content_id) + "_time_minute_form";
-        minuteData.value = document.getElementById(id_head + String(content_id) + "_time_minute").innerText;
-        document.getElementById("workflow_data").appendChild(titleData);
-        document.getElementById("workflow_data").appendChild(hourData);
-        document.getElementById("workflow_data").appendChild(minuteData);
-        mode = "";
-        content_id++;
+        if(mode == "add") { 
+            document.getElementById("registration_menu_" + mode).style.backgroundColor = "";
+            addForm(movingContent);
+            content_id++;
+            mode = "";
+        }
+        else if(mode == "move") {
+            document.getElementById(id_head + movingContent + "_margin_left_form").value = document.getElementById(id_head + movingContent).style.marginLeft;
+            document.getElementById(id_head + movingContent + "_margin_top_form").value = document.getElementById(id_head + movingContent).style.marginTop;
+            //関連するリンクを再生する
+            attachConnection(movingContent);
+        }
+        movingContent = "";
     };
 
     function editContent(value) {
-        if(mode != "edit" && mode != "connect") return;
+        if(mode != "edit" && mode != "move" && mode != "connect") return;
         if(editingContent != "") {
             finishEditContent(editingContent);
         }
@@ -109,6 +167,17 @@
             document.getElementById(value.id + "_title").innerHTML = "<input id='" + title_id + "' value='" + title + "'>";
             document.getElementById(value.id + "_time").innerHTML = "<input type='number' id='" + hour_id + "' value='" + hour + "' min=0 style='width: 20%' onchange='" + func + "'>時間&nbsp;<input type='number' id='"+ minute_id +"' value='" + minute + "' min=" + minute_min + " max='45' step='15' style='width: 20%'>分";
             value.setAttribute('onclick', '');
+        }
+        else if(mode == "move") {
+            if(movingContent == "") {
+                movingContent = value.id.substr(id_head.length);
+                document.getElementById("registration_free_field").addEventListener("mousemove", moveContent);
+                document.getElementById("registration_free_field").addEventListener("click", putContent);
+                //このクリックで配置する際の関数が呼ばれるのを防止する
+                block_once = true;
+                //関連するリンクの削除
+                removeConnection(movingContent);
+            }
         }
         else if(mode == "connect") {
             let input_id = value.id.substr(id_head.length);
@@ -152,74 +221,22 @@
                     }
                 }
                 if(!check) {
-                    //始点から見て左右上下どの方角かをまず決定する
-                    //それをもとに、
-                    let begin_x = Number(document.getElementById(id_head + connect_begin_id).style.marginLeft.substring(0,document.getElementById(id_head + connect_begin_id).style.marginLeft.length - 2));
-                    let begin_y = Number(document.getElementById(id_head + connect_begin_id).style.marginTop.substring(0,document.getElementById(id_head + connect_begin_id).style.marginTop.length - 2));
-                    let end_x = Number(document.getElementById(id_head + input_id).style.marginLeft.substring(0,document.getElementById(id_head + input_id).style.marginLeft.length - 2));
-                    let end_y = Number(document.getElementById(id_head + input_id).style.marginTop.substring(0,document.getElementById(id_head + input_id).style.marginTop.length - 2));
-                    let diff_x = end_x - begin_x;
-                    let diff_y = end_y - begin_y;
-                    let arrow_begin_x;
-                    let arrow_begin_y;
-                    let arrow_end_x;
-                    let arrow_end_y;
-                    let angle;
-                    //始点が上側の場合
-                    if(diff_y > 0) {
-                        arrow_begin_x = begin_x + 100;
-                        arrow_begin_y = begin_y + 75;
-                        arrow_end_x = end_x + 100;
-                        arrow_end_y = end_y;
-                    }
-                    //始点が下側の場合
-                    else {
-                        arrow_begin_x = begin_x + 100;
-                        arrow_begin_y = begin_y;
-                        arrow_end_x = end_x + 100;
-                        arrow_end_y = end_y + 75;
-                    }
-                    //左右でつなぐ場合
-                    if(Math.abs(diff_y) <= 75) {
-                        //始点が左側の場合
-                        if(diff_x > 0) {
-                            arrow_begin_x = begin_x + 200;
-                            arrow_begin_y = begin_y + 37.5;
-                            arrow_end_x = end_x;
-                            arrow_end_y = end_y + 37.5;
-                        }
-                        //始点が右側の場合
-                        else {
-                            arrow_begin_x = begin_x;
-                            arrow_begin_y = begin_y + 37.5;
-                            arrow_end_x = end_x + 200;
-                            arrow_end_y = end_y + 37.5;
-                        }
-                    }
-                    if(Math.abs(arrow_end_x - arrow_begin_x) == 0) angle = 90;
-                    else angle = Math.atan(Math.abs(arrow_end_y - arrow_begin_y) / Math.abs(arrow_end_x - arrow_begin_x)) / Math.PI * 180;
-                    //alert(diff_x);
-                    //alert(diff_y)
                     if(!(connect_begin_id in connect_relation)) {
                         connect_relation[connect_begin_id] = [];
                     }
+                    if(!(input_id in connect_relation_rev)) {
+                        connect_relation_rev[input_id] = [];
+                    }
                     connect_relation[connect_begin_id].push(input_id);
+                    connect_relation_rev[input_id].push(connect_begin_id);
                     let newRelation = document.createElement("input");
                     newRelation.type = "hidden";
                     newRelation.name = "connection[][" + connect_begin_id + "][" + input_id + "]";
                     newRelation.id = "connection_" + connect_begin_id + "_" + input_id + "_form";
                     newRelation.value = true;
                     document.getElementById("workflow_data").appendChild(newRelation);
-                    let newArrow = document.createElement("div");
-                    newArrow.id = "connection_" + connect_begin_id + "_" + input_id;
-                    newArrow.className = "connection_arrow";
-                    newArrow.style.marginLeft = String(arrow_begin_x < arrow_end_x ? arrow_begin_x : arrow_end_x) + "px";
-                    newArrow.style.marginTop = String(arrow_begin_y < arrow_end_y ? arrow_begin_y : arrow_end_y) + "px";
-                    newArrow.style.width = String(Math.abs(arrow_end_x - arrow_begin_x)) + "px";
-                    newArrow.style.height = String(Math.abs(arrow_end_y - arrow_begin_y)) + "px";
-                    newArrow.style.backgroundImage = "linear-gradient("+ ((arrow_end_x - arrow_begin_x) * (arrow_end_y - arrow_begin_y) > 0 ? "" : "-") + angle + "deg, transparent 49%, black 49%, black 51%, transparent 51%)";
-                    document.getElementById("registration_free_field").appendChild(newArrow);
-                    //矢印の始点と終点について、決定する必要がある
+                    //矢印を生成
+                    createArrow(connect_begin_id, input_id);
                     document.getElementById(id_head + connect_begin_id).style.borderColor = "black";
                     connect_begin_id = "";
                 }           
@@ -263,6 +280,7 @@
         mode = modeName;
         document.getElementById("registration_menu_" + mode).style.backgroundColor = "white";
         if(mode == "add") {
+            movingContent = String(content_id);
             let new_content = document.createElement("div");
             new_content.className = "registration_free_field_content";
             new_content.id = id_head + String(content_id);
@@ -306,6 +324,63 @@
         if(document.getElementById(value_id + "_time_input_minute").value == "") {
             document.getElementById(value_id + "_time_input_minute").value = 0;
         }
+    }
+
+    function createArrow(start, end) {
+        let begin_x = Number(document.getElementById(id_head + start).style.marginLeft.substring(0,document.getElementById(id_head + start).style.marginLeft.length - 2));
+        let begin_y = Number(document.getElementById(id_head + start).style.marginTop.substring(0,document.getElementById(id_head + start).style.marginTop.length - 2));
+        let end_x = Number(document.getElementById(id_head + end).style.marginLeft.substring(0,document.getElementById(id_head + end).style.marginLeft.length - 2));
+        let end_y = Number(document.getElementById(id_head + end).style.marginTop.substring(0,document.getElementById(id_head + end).style.marginTop.length - 2));
+        let diff_x = end_x - begin_x;
+        let diff_y = end_y - begin_y;
+        let arrow_begin_x;
+        let arrow_begin_y;
+        let arrow_end_x;
+        let arrow_end_y;
+        let angle;
+        //始点が上側の場合
+        if(diff_y > 0) {
+            arrow_begin_x = begin_x + 100;
+            arrow_begin_y = begin_y + 75;
+            arrow_end_x = end_x + 100;
+            arrow_end_y = end_y;
+        }
+        //始点が下側の場合
+        else {
+            arrow_begin_x = begin_x + 100;
+            arrow_begin_y = begin_y;
+            arrow_end_x = end_x + 100;
+            arrow_end_y = end_y + 75;
+        }
+        //左右でつなぐ場合
+        if(Math.abs(diff_y) <= 75) {
+            //始点が左側の場合
+            if(diff_x > 0) {
+                arrow_begin_x = begin_x + 200;
+                arrow_begin_y = begin_y + 37.5;
+                arrow_end_x = end_x;
+                arrow_end_y = end_y + 37.5;
+            }
+            //始点が右側の場合
+            else {
+                arrow_begin_x = begin_x;
+                arrow_begin_y = begin_y + 37.5;
+                arrow_end_x = end_x + 200;
+                arrow_end_y = end_y + 37.5;
+            }
+        }
+        if(Math.abs(arrow_end_x - arrow_begin_x) == 0) angle = 90;
+        else angle = Math.atan(Math.abs(arrow_end_y - arrow_begin_y) / Math.abs(arrow_end_x - arrow_begin_x)) / Math.PI * 180;
+        let newArrow = document.createElement("div");
+        newArrow.id = "connection_" + start + "_" + end;
+        newArrow.className = "connection_arrow";
+        newArrow.style.marginLeft = String(arrow_begin_x < arrow_end_x ? arrow_begin_x : arrow_end_x) + "px";
+        newArrow.style.marginTop = String(arrow_begin_y < arrow_end_y ? arrow_begin_y : arrow_end_y) + "px";
+        newArrow.style.width = String(Math.abs(arrow_end_x - arrow_begin_x)) + "px";
+        newArrow.style.height = String(Math.abs(arrow_end_y - arrow_begin_y)) + "px";
+        newArrow.style.backgroundImage = "linear-gradient("+ ((arrow_end_x - arrow_begin_x) * (arrow_end_y - arrow_begin_y) > 0 ? "" : "-") + angle + "deg, transparent 49%, black 49%, black 51%, transparent 51%)";
+        document.getElementById("registration_free_field").appendChild(newArrow);
+        //矢印の始点と終点について、明示的に示す必要がある
     }
 
 </script>
